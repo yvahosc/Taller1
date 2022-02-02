16ec4ae37c505fc16027d9f21358907cc944b2a1
yvahosc <yvahosc@unal.edu.co>
2022-02-02 12:33:04 -0500

    Creación de la clase ContactInformation
    Creación de la clase Información de contacto que extiende de la clase persona, con los atributos dirección y correo electrónico y los métodos get y set para cada uno de los atributos.

---
+++ src/Clases/ContactInformation.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Yeisson Vahos Cortes
+ */
+public class ContactInformation extends Person{
+    
+    private String address;
+    private String email;    
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
