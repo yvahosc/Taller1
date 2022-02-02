43369352797534f99ba217c3aba68a6eab1e0da6
yvahosc <yvahosc@unal.edu.co>
2022-02-01 21:12:53 -0500

    Creación de la clase Person
    Creación de la clase persona con la información inicial dada en el taller práctico; con los atributos name, lastName1, lastName2, dateBirth, height y los métodos get y set para cada uno de los atributos.

---
+++ src/Clases/Person.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Yeisson Vahos Cortes
+ */
+public class Person {
+    public String name;
+    public String lastName1;
+    public String lastName2;
+    public Date dateBirth;
+    public float height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName1() {
+        return lastName1;
+    }
+
+    public void setLastName1(String lastName1) {
+        this.lastName1 = lastName1;
+    }
+
+    public String getLastName2() {
+        return lastName2;
+    }
+
+    public void setLastName2(String lastName2) {
+        this.lastName2 = lastName2;
+    }
+
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+    
+    
+}
