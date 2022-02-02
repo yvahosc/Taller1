5e164d2b59f2ddda52b4d9f28c298d709a5d0a3d
yvahosc <yvahosc@unal.edu.co>
2022-02-02 13:14:16 -0500

    Creación de la clase Features
    Creación de la clase características que extiende de la clase fruta, con los atributos origen, precio y tipo y los métodos get y set para cada uno de los atributos.

---
+++ src/Clases/Features.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Yeisson Vahos Cortes
+ */
+public class Features extends Fruit{
+    public String origin;
+    public int price;
+    public String type;
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+    
+}
